e904c693d826907ae0daabb5d90b87be4c151e90
silas <silasfrigo@gmail.com>
2021-03-07 19:54:38 -0300

    refactor: main file

--- src/project/prescriptions/main.py
+++ src/project/prescriptions/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .api.api_v1.api import router
 from mangum import Mangum
+from prescriptions.models import prescription
+from connections import engine
 
+
+prescription.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title='IClinic Prescriptions API',
