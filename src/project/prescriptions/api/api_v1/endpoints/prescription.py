6cd7a4c330f8a6a51bf90569a4e105910ffde24c
silas <silasfrigo@gmail.com>
2021-03-07 19:53:06 -0300

    feat: removing simple endpoint and finishing prescription

--- src/project/prescriptions/api/api_v1/endpoints/prescription.py
+++ src/project/prescriptions/api/api_v1/endpoints/prescription.py
@@ -1,16 +1,21 @@
-from fastapi import APIRouter, Response
+from aws_lambda_powertools import Logger
+from fastapi import APIRouter, Depends
+from prescriptions.schemas.prescription import CreatePrescription, ReadPrescription
+from prescriptions.schemas.base import Result
+from sqlalchemy.orm import Session
+from prescriptions.get_db import get_db
+from prescriptions.crud.prescription import ProcessPrescription
+
+
+logger = Logger()
 router = APIRouter()
 
 
-@router.get('/simple')
-async def hello_world(response: Response):
+@router.post('/prescriptions', response_model=Result[ReadPrescription])
+async def create_prescription(prescription_in: CreatePrescription, db: Session = Depends(get_db)):
+    process_prescription = ProcessPrescription(db=db)
+    created = process_prescription.process(prescription_in)
+    logger.info({'info': 'prescription_created_successfully', 'attr': created.__dict__})
     return {
-        "hello": "world"
-    }
-
-
-@router.post('/prescriptions')
-async def create_prescription(response: Response):
-    return {
-        "foo": "bar"
+        "data": created
     }
